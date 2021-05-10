3e3c9d4e44297c4f6456efadb845bbf4d64eb2f0
JannikSW <jannik.scheller@wtnet.de>
2021-05-10 09:55:25 +0200

    Moved getCharge-Function to Price-Class

--- src/Movie.java
+++ src/Movie.java
@@ -31,23 +31,7 @@ public class Movie {
     }
     private Price price;
 	double getCharge(Rental daysRented) {
-	    double result = 0;
-	    switch (daysRented.getMovie().getPriceCode()) {
-	        case Movie.REGULAR:
-	            result += 2;
-	            if (daysRented.getDaysRented() > 2)
-	                result += (daysRented.getDaysRented() - 2) * 1.5;
-	            break;
-	        case Movie.NEW_RELEASE:
-	            result += daysRented.getDaysRented() * 3;
-	            break;
-	        case Movie.CHILDRENS:
-	            result += 1.5;
-	            if (daysRented.getDaysRented() > 3)
-	                result += (daysRented.getDaysRented() - 3) * 1.5;
-	            break;
-	    }
-	    return result;
+		return price.getCharge(daysRented);
 	}
 	int getFrequentRenterPoints(Rental daysRented) {
 		if ((daysRented.getMovie().getPriceCode() == Movie.NEW_RELEASE) && daysRented.getDaysRented() > 1) 
--- src/Price.java
+++ src/Price.java
@@ -1,5 +1,25 @@
 abstract class Price {
 	abstract int getPriceCode();
+
+	double getCharge(Rental daysRented) {
+	    double result = 0;
+	    switch (daysRented.getMovie().getPriceCode()) {
+	        case Movie.REGULAR:
+	            result += 2;
+	            if (daysRented.getDaysRented() > 2)
+	                result += (daysRented.getDaysRented() - 2) * 1.5;
+	            break;
+	        case Movie.NEW_RELEASE:
+	            result += daysRented.getDaysRented() * 3;
+	            break;
+	        case Movie.CHILDRENS:
+	            result += 1.5;
+	            if (daysRented.getDaysRented() > 3)
+	                result += (daysRented.getDaysRented() - 3) * 1.5;
+	            break;
+	    }
+	    return result;
+	}
 }
 
 class ChildrensPrice extends Price {
